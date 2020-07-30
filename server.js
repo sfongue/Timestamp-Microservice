9b846fe4f0a722f70db5fc12823dab570b2241a7
sfongue <sfongue@gmail.com>
2020-07-30 16:13:42 +0200

    add exception for an invalid date, add condition if unix date

--- server.js
+++ server.js
@@ -25,11 +25,50 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/:date_string", (req, res) => {
-  var date = req.params.date_string;
+app.get("/api/timestamp/", (req, res) => {
+  var date = new Date();
   var unix_tm = new Date(date).getTime();
   var utc_tm = new Date(date).toUTCString();
   res.json({"unix": unix_tm, "utc": utc_tm})
+})
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  var date = req.params.date_string;
+  if(isNaN(date) === true) {
+    var unix_tm = new Date(date).getTime();
+    var utc_tm = new Date(date).toUTCString();
+  } else {
+    date = parseInt(date);
+    var unix_tm = date;
+    //
+    var a = new Date(date);
+    var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var day = days[a.getDay()];
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    if (hour.toString().length < 2) {
+      hour = ('0' + hour).slice(-2);
+      console.log('hour');
+    }
+    if (min.toString().length < 2) {
+      min = ('0' + min).slice(-2);
+    }
+    if (sec.toString().length < 2) {
+      sec = ('0' + sec).slice(-2);
+    }
+    var utc_tm = day + ', '+ date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec + ' GMT';
+  }
+  console.log(unix_tm.toString())
+  if (isNaN(new Date(date).getTime())) {
+    res.json({"error" : "Invalid Date" });
+  } else {
+    res.json({"unix": unix_tm, "utc": utc_tm})
+  }
 })
 
 // listen for requests :)
